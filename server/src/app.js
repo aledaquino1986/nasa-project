c3d3250cd4b750f09cf4a2b108914ff14c846647
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-07-16 17:24:35 -0300

    Cors issue solved

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const planetRouter = require("./routes/planets/planets.router")
+const cors = require("cors")
 const app = express()
+
+
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
 
 
 app.use(express.json())
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,6 +1,7 @@
 const planets = require("../../models/planets.model")
 
 function getAllPlanets(req, res) {
+  console.log("llego")
     return res.status(200).json(planets);
 }
 
