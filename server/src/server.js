32d49f8caae518b56e47a9a3872606b6574c9b3c
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-07-19 18:56:59 -0300

    add data on startup

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,8 @@
 const http = require("http");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
-
-const server = http.createServer(app);
-
 const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
 
 async function startServer() {
   await loadPlanetsData();
