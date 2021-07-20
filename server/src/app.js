871284742c0a0354fc19269b76b8c5800dc495cc
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-07-20 14:32:35 -0300

    serve react files in backend. Use scripts to automate process

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,23 @@
-const express = require("express")
-const planetRouter = require("./routes/planets/planets.router")
-const cors = require("cors")
-const app = express()
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
 
+const planetRouter = require("./routes/planets/planets.router");
 
-app.use(cors({
-  origin: "http://localhost:3000"
-}))
+const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000"
+  })
+);
 
-app.use(express.json())
-app.use(planetRouter)
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.use(planetRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "public", "index.html"));
+});
 
-module.exports = app
+module.exports = app;
